b56538a28ca38a32933f1be4f62aee86ff4f26f8
fyfyzzk <fyfyzzk@hotmail.com>
2018-10-17 22:57:43 -0700

    104 dfs

---
+++ leetcode/104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,101 @@
+/*
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (57.17%)
+ * Total Accepted:    392.9K
+ * Total Submissions: 686.6K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+/*
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (57.17%)
+ * Total Accepted:    392.9K
+ * Total Submissions: 686.6K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    function helper(root) {
+        if (root === null) {
+            return 0;
+        }
+
+        if (root.left === null && root.right === null) {
+            return 1;
+        }
+
+        return Math.max(helper(root.left), helper(root.right)) + 1;
+    }
+
+    return helper(root);
+};
