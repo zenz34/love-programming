3c8a0fd9aa1d721d3138710cd0b1f1473a452db4
fyfyzzk <fyfyzzk@hotmail.com>
2018-10-20 23:49:41 -0700

    1 hash, sort, 6 math

---
+++ leetcode/1.two-sum.js
@@ -0,0 +1,37 @@
+/*
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (38.95%)
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
