a034726df51b3e089d9f8be390e8a15c31dc2e40
fyfyzzk <fyfyzzk@hotmail.com>
2019-10-24 21:29:55 -0700

    practice more. Base case, recurrence relation

---
+++ leetcode/617.merge-two-binary-trees.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=617 lang=java
+ *
+ * [617] Merge Two Binary Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+
+        if (t2 == null) {
+            return t1;
+        }
+
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
+// @lc code=end
+
+/*
+ * base case: 1.both are null; 2.one of them are null only take the valid one's
+ * value Recurrence Relation: 1.add their value; 2.merge t1's and t2's left tree
+ * and right tree
+ * 
+ * 
+ * 
+ */
